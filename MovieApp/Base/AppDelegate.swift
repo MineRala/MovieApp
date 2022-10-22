c0518a0d005fc0e8058c7aeee791434a86078094
Mine Rala <mine-rala@hotmail.com>
2022-10-22 19:19:34 +0300

    Added firebase notificarion

--- MovieApp/Base/AppDelegate.swift
+++ MovieApp/Base/AppDelegate.swift
@@ -7,28 +7,50 @@
 
 import UIKit
 import Firebase
+import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    // MARK: - NOTE
+    // Apple Developer hesabım olmadığı için apns key yoktur.
+    // Notification için kod implemente edildi.
+    // Eğer firebase client'ına apns key girilirse push notification atacaktır.
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
+        MovieNotificationManager.shared.requestNotifications(application)
         return true
     }
 
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
+extension AppDelegate: UNUserNotificationCenterDelegate {
+ 
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        MovieNotificationManager.shared.setDeviceToken(deviceToken: deviceToken)
+    }
+    
+    func application(_ application: UIApplication, didFailToContinueUserActivityWithType userActivityType: String, error: Error) {
+        print("Failed to register with push")
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        print("Will gets called when app is in foreground and we want to show banner ")
+        completionHandler([.banner, .sound, .badge])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+        print("Will gets called when user tap on notification")
+        completionHandler()
+    }
+}
---
+++ MovieApp/Helpers/MovieNotificationManager.swift
@@ -0,0 +1,64 @@
+//
+//  MovieNotificationManager.swift
+//  MovieApp
+//
+//  Created by Mine Rala on 22.10.2022.
+//
+
+import UIKit
+import UserNotifications
+import FirebaseMessaging
+
+// MARK: - NOTE
+// Apple Developer hesabım olmadığı için apns key yoktur.
+// Notification için kod implemente edildi.
+// Eğer firebase client'ına apns key girilirse push notification atacaktır.
+
+public protocol MovieNotificationManagerDelegate: AnyObject {
+    func getDeviceToken(token: String)
+}
+
+final class MovieNotificationManager: NSObject, MessagingDelegate {
+    static let shared = MovieNotificationManager()
+    private(set) var deviceToken: Data?
+    private(set) var authorizationOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+    public weak var delegate: MovieNotificationManagerDelegate?
+    let notificationCenter = UNUserNotificationCenter.current()
+    private(set) var authorizationStatus: UNAuthorizationStatus = .notDetermined
+    
+    private override init() {
+        super.init()
+        Messaging.messaging().delegate = self
+    }
+    
+    public func setDeviceToken(deviceToken: Data) {
+        self.deviceToken = deviceToken
+    }
+    
+    public func setAuthorizationOptions(authorizationOptions: UNAuthorizationOptions) {
+        self.authorizationOptions = authorizationOptions
+    }
+
+    public func getAuthorizationStatus() -> UNAuthorizationStatus {
+        return self.authorizationStatus
+    }
+    
+    public func requestNotifications(_ application: UIApplication, completionHandler: ((Bool, UNAuthorizationStatus) -> Void)? = nil) {
+        self.notificationCenter.requestAuthorization(options: authorizationOptions) { [weak self] (granted, error) in
+            self?.notificationCenter.getNotificationSettings { (settings) in
+                let status = settings.authorizationStatus
+                self?.authorizationStatus = status
+                if let error = error {
+                    print("requestAuthorization error:\(error)")
+                }
+                if granted {
+                    DispatchQueue.main.async {
+                        UIApplication.shared.registerForRemoteNotifications()
+                        print("register notification")
+                    }
+                }
+                completionHandler?(granted, status)
+            }
+        }
+    }
+}
